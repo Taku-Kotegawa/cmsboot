0724d89b389f066d4bf224ea1a64ac7d2c4901a3
T.Kotegawa <tkotegawa@stnet.co.jp>
2021-05-16 09:34:46 +0900

    first commit

---
+++ src/main/java/jp/co/stnet/cms/CmsApplication.java
@@ -0,0 +1,13 @@
+package jp.co.stnet.cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/co/stnet/cms/CmsApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.stnet.cms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
