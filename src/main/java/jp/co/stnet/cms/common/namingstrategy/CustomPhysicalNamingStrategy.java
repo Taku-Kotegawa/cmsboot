fbb1dda846c059ec6b27a4a6504e971fa69ef335
T.Kotegawa <tkotegawa@stnet.co.jp>
2021-05-17 07:46:14 +0900

    共通処理(JAPの命名戦略)

---
+++ src/main/java/jp/co/stnet/cms/common/namingstrategy/CustomPhysicalNamingStrategy.java
@@ -0,0 +1,45 @@
+package jp.co.stnet.cms.common.namingstrategy;
+
+import org.hibernate.boot.model.naming.Identifier;
+import org.hibernate.boot.model.naming.PhysicalNamingStrategy;
+import org.hibernate.engine.jdbc.env.spi.JdbcEnvironment;
+
+/**
+ * Spring Data JPA モデルからテーブルを生成する際のカスタマイズ命名規則(大文字のスネークケース)
+ */
+public class CustomPhysicalNamingStrategy implements PhysicalNamingStrategy {
+    
+    private Identifier convertToSnakeCase(final Identifier identifier) {
+        final String regex = "([a-z])([A-Z])";
+        final String replacement = "$1_$2";
+        final String newName = identifier.getText()
+                .replaceAll(regex, replacement)
+                .toUpperCase();
+        return Identifier.toIdentifier(newName);
+    }
+
+    @Override
+    public Identifier toPhysicalCatalogName(Identifier identifier, JdbcEnvironment jdbcEnvironment) {
+        return identifier;
+    }
+
+    @Override
+    public Identifier toPhysicalSchemaName(Identifier identifier, JdbcEnvironment jdbcEnvironment) {
+        return identifier;
+    }
+
+    @Override
+    public Identifier toPhysicalTableName(Identifier identifier, JdbcEnvironment jdbcEnvironment) {
+        return convertToSnakeCase(identifier);
+    }
+
+    @Override
+    public Identifier toPhysicalSequenceName(Identifier identifier, JdbcEnvironment jdbcEnvironment) {
+        return convertToSnakeCase(identifier);
+    }
+
+    @Override
+    public Identifier toPhysicalColumnName(Identifier identifier, JdbcEnvironment jdbcEnvironment) {
+        return convertToSnakeCase(identifier);
+    }
+}
